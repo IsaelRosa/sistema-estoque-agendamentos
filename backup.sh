3eeb1ae2e3d303590f55f06d8700714111ff2b0a
Isael Aparecido Rosa <isaelrosa@ufla.br>
2024-12-29 10:55:41 -0300

    Revert "Primeiro commit"
    This reverts commit d40476c43e810f8a65755ff5800fc0cc0844915f.

--- backup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-diretorio_backup="/home/isael/devops"
-nome_arquivo="backup_$(date +%Y%m%d_%H%M%S).tar.gz"
-tar -czf "$nome_arquivo" "diretorio_backup"
-echo "Backup concluido em $nome_arquivo"
